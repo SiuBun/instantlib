2d28b12b4f0c476748c01fbe5ad689b713bd3119
wangshaobin <wangshaobin@dituiba.com>
2019-07-08 15:28:44 +0800

    调整sp逻辑

---
+++ instantlib/src/main/java/com/baselib/instant/provider/sp/BundleCursor.java
@@ -0,0 +1,27 @@
+package com.baselib.instant.provider.sp;
+
+import android.database.MatrixCursor;
+import android.os.Bundle;
+
+/**
+ * @author wsb
+ */
+final class BundleCursor extends MatrixCursor {
+    private Bundle mBundle;
+
+    public BundleCursor(Bundle extras) {
+        super(new String[]{}, 0);
+        mBundle = extras;
+    }
+
+    @Override
+    public Bundle getExtras() {
+        return mBundle;
+    }
+
+    @Override
+    public Bundle respond(Bundle extras) {
+        mBundle = extras;
+        return mBundle;
+    }
+}
